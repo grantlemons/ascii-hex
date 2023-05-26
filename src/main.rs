910af390880676feab3f753e4f52dfb67d3be7e8
Grant Lemons <grant0lemons@gmail.com>
2023-05-25 21:04:37 -0500

    rework logic to respect newlines

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ struct Args {
 fn main() {
     let cli = Args::parse();
 
-    let text = cli.strings.join("\n");
-    let bytes = text.into_bytes();
+    let text = cli
+        .strings
+        .into_iter()
+        .map(|t| bytes_to_string(&t.into_bytes()))
+        .collect::<Vec<String>>()
+        .join("\n");
 
-    let output = bytes_to_string(&bytes);
-    println!("{output}");
+    println!("{text}");
 }
 
 fn bytes_to_string(bytes: &[u8]) -> String {
