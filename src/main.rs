324742576d8c94187ea138eb6ae68adb4ac7ade5
Grant Lemons <grant0lemons@gmail.com>
2023-05-25 20:21:26 -0500

    create args struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    string: String,
+    other_strings: Option<Vec<String>>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
