fcf386aba38c1f11bee52bed219017e77e8b40ac
Grant Lemons <grant0lemons@gmail.com>
2023-05-25 22:45:54 -0500

    implement to-hex and to-ascii modes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
+use std::{fs::File, str::FromStr};
 
 use clap::{Parser, ValueEnum};
 
@@ -8,6 +8,7 @@ use clap::{Parser, ValueEnum};
 #[command(author, version, about, long_about = None)]
 struct Args {
     // Directionality of conversion
+    #[arg(value_enum)]
     mode: Mode,
 
     // Input strings
@@ -30,20 +31,56 @@ fn main() {
     let cli = Args::parse();
 
     let mut output = String::new();
-    if let Some(strings) = cli.strings {
-        output = process_strings(strings);
-    } else if let Some(file_path) = cli.file {
-        let bytes = File::open(file_path).unwrap().bytes();
-        output = bytes_to_string(bytes.into_iter().filter_map(|b| b.ok()));
+
+    match cli.mode {
+        Mode::ToHex => {
+            if let Some(strings) = cli.strings {
+                output = process_to_hex(strings);
+            } else if let Some(file_path) = cli.file {
+                let bytes = File::open(file_path).unwrap().bytes();
+                output = bytes_to_string(bytes.into_iter().filter_map(|b| b.ok()));
+            }
+        }
+        Mode::ToASCII => {
+            if let Some(strings) = cli.strings {
+                output = process_to_ascii(strings.into_iter());
+            } else if let Some(file_path) = cli.file {
+                let mut contents = String::new();
+                File::open(file_path)
+                    .unwrap()
+                    .read_to_string(&mut contents)
+                    .unwrap();
+                let ascii_lines: Vec<String> = contents
+                    .split('\n')
+                    .filter(|l| !l.is_empty())
+                    .map(|s| String::from_str(s).unwrap())
+                    .collect();
+                println!("{:?}", ascii_lines);
+                output = process_to_ascii(ascii_lines.into_iter());
+            }
+        }
     }
 
     println!("{output}");
 }
 
-fn process_strings(vec: Vec<String>) -> String {
+fn process_to_ascii<T: Iterator<Item = String>>(lines: T) -> String {
+    lines
+        .map(|l| {
+            String::from_iter(
+                l.split(' ')
+                    .filter(|c| !c.is_empty())
+                    .map(|c| u8::from_str_radix(c, 16).unwrap() as char),
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+fn process_to_hex(vec: Vec<String>) -> String {
     vec.into_iter()
         .map(|t| bytes_to_string(t.into_bytes().into_iter()))
-        .collect::<Vec<String>>()
+        .collect::<Vec<_>>()
         .join("\n")
 }
 
