710eeaee56fd45c62c47e8cf7f9c31c1886181b0
Grant Lemons <grant0lemons@gmail.com>
2023-05-25 20:39:48 -0500

    build out main functionality

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    string: String,
-    other_strings: Option<Vec<String>>,
+    strings: Vec<String>,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+
+    let text = cli.strings.join("\n");
+    let bytes = text.into_bytes();
+
+    let output = bytes_to_string(&bytes);
+    println!("{output}");
+}
+
+fn bytes_to_string(bytes: &[u8]) -> String {
+    bytes
+        .into_iter()
+        .map(|b| format!("{:02X}", b))
+        .collect::<Vec<_>>()
+        .join(" ")
 }
