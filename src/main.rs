99d72111f4699e612bdc6a3d8c1187efccaffd4a
Grant Lemons <grant0lemons@gmail.com>
2023-05-28 14:32:27 -0500

    add more options for input and output

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,14 @@ struct Args {
     // Input file path
     #[arg(short, long, value_name = "FILE")]
     file: Option<PathBuf>,
+
+    // Spaces in output
+    #[arg(short, long)]
+    compact: bool,
+
+    // Lowercase output
+    #[arg(short, long)]
+    lower: bool,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -35,10 +43,14 @@ fn main() {
     match cli.mode {
         Mode::ToHex => {
             if let Some(strings) = cli.strings {
-                output = process_to_hex(strings);
+                output = process_to_hex(strings, !cli.compact, cli.lower);
             } else if let Some(file_path) = cli.file {
                 let bytes = File::open(file_path).unwrap().bytes();
-                output = bytes_to_string(bytes.into_iter().filter_map(|b| b.ok()));
+                output = bytes_to_string(
+                    bytes.into_iter().filter_map(|b| b.ok()),
+                    !cli.compact,
+                    cli.lower,
+                );
             }
         }
         Mode::ToASCII => {
@@ -68,8 +80,8 @@ fn process_to_ascii<T: Iterator<Item = String>>(lines: T) -> String {
     lines
         .map(|l| {
             String::from_iter(
-                l.split(' ')
-                    .filter(|c| !c.is_empty())
+                extract_pairs(l)
+                    .iter()
                     .map(|c| u8::from_str_radix(c, 16).unwrap() as char),
             )
         })
@@ -77,16 +89,35 @@ fn process_to_ascii<T: Iterator<Item = String>>(lines: T) -> String {
         .join("\n")
 }
 
-fn process_to_hex(vec: Vec<String>) -> String {
+fn extract_pairs(input: String) -> Vec<String> {
+    input
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<Vec<_>>()
+        .chunks(2)
+        .map(|c| String::from_iter(c))
+        .collect()
+}
+
+fn process_to_hex(vec: Vec<String>, spaces: bool, lower: bool) -> String {
     vec.into_iter()
-        .map(|t| bytes_to_string(t.into_bytes().into_iter()))
+        .map(|t| bytes_to_string(t.into_bytes().into_iter(), spaces, lower))
         .collect::<Vec<_>>()
         .join("\n")
 }
 
-fn bytes_to_string<T: Iterator<Item = u8>>(bytes: T) -> String {
+fn bytes_to_string<T: Iterator<Item = u8>>(bytes: T, spaces: bool, lower: bool) -> String {
+    let seperator = if spaces { " " } else { "" };
+    let format_fn = |b| {
+        if lower {
+            format!("{:02x}", b)
+        } else {
+            format!("{:02X}", b)
+        }
+    };
+
     bytes
-        .map(|b| format!("{:02X}", b))
+        .map(|b| format_fn(b))
         .collect::<Vec<_>>()
-        .join(" ")
+        .join(seperator)
 }
